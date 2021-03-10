f59d79edf7b0b78561084cbca2c37b02fdaaf7ee
defolt-12 <d.e.f.o.l.t@yandex.ru>
2021-03-10 16:43:53 +0300

    create: map

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+});
+
