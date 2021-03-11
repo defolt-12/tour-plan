bfa2a7be55514fbf5de4d4d11b67466670eb0d53
defolt-12 <71543079+defolt-12@users.noreply.github.com>
2021-03-11 20:50:34 +0300

    create: Сдайдер с отзывом

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
-const swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper('.hotel-slider', {
+  // Optional parameters
+  loop: true,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+});
+
+const rew = new Swiper('.reviews-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    prevEl: '.reviews-slider__button--prev',
+    nextEl: '.reviews-slider__button--next',
   },
 });
