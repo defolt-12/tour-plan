3b3be40c95e7c634187d89c8d6466763dbd09ae7
defolt-12 <71543079+defolt-12@users.noreply.github.com>
2021-03-17 14:47:00 +0300

    create: footer

--- js/main.js
+++ js/main.js
@@ -24,3 +24,19 @@ const rew = new Swiper('.reviews-slider', {
     nextEl: '.reviews-slider__button--next',
   },
 });
+
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [7.8905900, 98.3981000],
+            zoom: 15
+          });
+};
+
+let paralax = document.getElementById('newsletter')
+  this.addEventListener('scroll', function () {
+    // paralax.style.opacity = 1 - +this.pageYOffset/800+''
+    paralax.style.top = +this.pageYOffset/500+'2px'
+    paralax.style.backgroundPositionY = - + +this.pageYOffset/3.5+'px'
+});
