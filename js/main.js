ef26245ad94632c432468ce9bd9b01eb35c35517
defolt-12 <71543079+defolt-12@users.noreply.github.com>
2021-04-05 15:35:29 +0300

    Создание Модального окна
    Создание Модального окна

--- js/main.js
+++ js/main.js
@@ -47,3 +47,79 @@ menuButton.addEventListener('click', function () {
   document.querySelector(".nav")
   .classList.toggle("nav--visible");
 });
+
+
+// let modalOverlay = document.querySelector(".bookig__button");
+// modalOverlay.addEventListener('click', function () {
+//   console.log("Клик по кнопке меню");
+//   document.querySelector(".modal__overlay")
+//   .classList.add("modal__overlay--visible");
+// });
+
+let modalDialog = document.querySelector(".button");
+modalDialog.addEventListener('click', function () {
+  console.log("Клик по кнопке меню");
+  document.querySelector(".modal__dialog").classList.add("modal__dialog--visible");
+  document.querySelector(".modal__overlay").classList.add("modal__overlay--visible");
+});
+
+// let modalOverlay = document.querySelector(".button");
+// modalOverlay.addEventListener('click', function () {
+//   console.log("Клик по кнопке меню");
+//   document.querySelector(".modal__overlay")
+//   .classList.toggle("modal__overlay--visible");
+// });
+
+
+
+
+// let btns          = document.querySelectorAll('.button');
+// let modalOverlay  = document.querySelector('.modal__overlay');
+// let modalDialog   = document.querySelector('.modal__dialog');
+// // let modalClose    = document.querySelector('.modal__close');
+// // let modals = document.querySelector('.modal');
+
+// btns.forEach((el) => {
+// 	el.addEventListener('click', (e) => {
+// 		let path = e.currentTarget.getAttribute('data-path');
+//     document.querySelector(`[data-target="${path}"]`).classList.toggle('modal__dialog--visible');
+//     modalDialog.classList.toggle('modal__dialog--visible');
+// 		// modals.forEach((el) => {
+// 		// 	el.classList.remove('modal__dialog--visible');
+// 		// });
+
+// 	});
+// });
+
+// let close = document.querySelector('.modal__close');
+//   close.addEventListener('click' , function () {
+//     modalOverlay.style.display = 'none',
+//     modalDialog.style.display = 'none'
+//   });
+
+//   let closeOverlay = document.querySelector('.modal__overlay');
+//   closeOverlay.addEventListener('click' , function () {
+//     modalOverlay.style.display = 'none',
+//     modalDialog.style.display = 'none'
+//   });
+
+// modalDialog.addEventListener('click', function(e) {
+//   document.querySelector('.modal__dialog--visible').classList.remove('visible');
+//   this.classList.remove('active');
+// });
+// modalClose.addEventListener('click', (e) => {
+
+//   // if (e.target == modalOverlay) {
+// 	// 	modalOverlay.classList.remove('modal__dialog--visible');
+//   // modalClose.forEach((el) => {
+// 	// 		el.classList.remove('modal__dialog--visible');
+// 	// 	});
+// 	// }
+
+
+//   // modals.forEach((el) => {
+//   //   el.classList.remove('modal--visible')
+//   // });
+//   modalClose.classList.remove('modal__dialog--visible');
+//   // modalOverlay.classList.remove('modal__overlay--visible');
+// });
